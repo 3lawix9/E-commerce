f0f0ff5a9879368ac435366db9d9917219a869cf
3lawix9 <92171974+3lawix9@users.noreply.github.com>
2023-08-17 11:42:00 +0300

    fix deploy issues

--- Components/Index.js
+++ Components/Index.js
@@ -32,31 +32,47 @@ export default function Index() {
     <Input
       type="text"
       value={search}
-      onChange={e => setSearch(e.target.value)}
+      onChange={(e) => setSearch(e.target.value)}
       placeholder="Search for products..."
-      className="bg-gray-200 rounded-xl p-3 w-full text-black"
+      className="bg-white text-gray-800 rounded-full py-2 px-4 w-full focus:outline-none shadow-sm"
     />
   );
-
+  
   const navbar = (
-    <Navbar className=" sticky mx-auto max-w-screen-xl px-4 py-3 border-0">
-      <div className="flex flex-wrap items-center justify-between gap-y-4 text-blue-gray-900">
+    <Navbar className="sticky top-0 left-0 right-0  px-4 py-3 border-b border-gray-300">
+      <div className="flex items-center justify-between">
         <Typography
           as="a"
           href="#"
-          variant="h6"
-          className="mr-4  text-black font-bold cursor-pointer py-1.5"
+          variant="h4"
+          className="text-gray-800 font-bold cursor-pointer "
         >
           Electric Store
         </Typography>
-
-        <div className="relative flex w-full gap-0 md:w-max">
+  
+        <div className="relative flex w-full md:w-64">
           {input}
+          <button className="absolute right-2 top-1/2 transform -translate-y-1/2 text-gray-500 hover:text-gray-800 focus:outline-none">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-6 w-6"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={2}
+                d="M15 19l-7-7 7-7"
+              />
+            </svg>
+          </button>
         </div>
-       
       </div>
     </Navbar>
   );
+  
 
   return (
     <Layout>
